e752bb6984c631e03239969f577d2cdf92a4c0be
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2025-03-04 18:22:11 +0300

    initial commit

---
+++ UserList7AppsTests/UserList7AppsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserList7AppsTests.swift
+//  UserList7AppsTests
+//
+//  Created by Murat Ceyhun Korpeoglu on 4.03.2025.
+//
+
+import Testing
+@testable import UserList7Apps
+
+struct UserList7AppsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
